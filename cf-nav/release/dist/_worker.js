eb05ba10f90ad1aae938195495c6dd504d0347c9
erasmusxu-b <erasmus.xu@gmail.com>
2026-08-24 22:57:56 +0800

    fix(build): dist/_worker.js 改 ESM 格式(--format=esm)，修复模块加载崩溃导致整站 522

--- cf-nav/release/dist/_worker.js
+++ cf-nav/release/dist/_worker.js
@@ -1,28 +1,3 @@
-var __defProp = Object.defineProperty;
-var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-var __getOwnPropNames = Object.getOwnPropertyNames;
-var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __export = (target, all) => {
-  for (var name in all)
-    __defProp(target, name, { get: all[name], enumerable: true });
-};
-var __copyProps = (to, from, except, desc) => {
-  if (from && typeof from === "object" || typeof from === "function") {
-    for (let key of __getOwnPropNames(from))
-      if (!__hasOwnProp.call(to, key) && key !== except)
-        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
-  }
-  return to;
-};
-var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
-
-// functions/worker-entry.js
-var worker_entry_exports = {};
-__export(worker_entry_exports, {
-  default: () => worker_entry_default
-});
-module.exports = __toCommonJS(worker_entry_exports);
-
 // functions/constants.js
 var KV = {
   DATA: "nav_data",
@@ -1124,3 +1099,6 @@ async function routeRequest(request, env, ctx, url, path, method) {
   if (env?.ASSETS?.fetch) return env.ASSETS.fetch(request);
   return new Response("Not Found", { status: 404 });
 }
+export {
+  worker_entry_default as default
+};
